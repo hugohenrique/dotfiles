93f5e93d4bf290e7a3a92af0b9d49b114169196a
Hugo Henrique <contato@hugohenrique.com.br>
2020-12-02 10:17:34 -0300

    Fix oh-my-osh directory path

--- shared/export.sh
+++ shared/export.sh
@@ -30,3 +30,4 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=~/.composer/vendor/bin:$PATH
