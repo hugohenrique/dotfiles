36540c018517a21eff714f5e754c95e9d748384b
Hugo Henrique <contato@hugohenrique.com.br>
2021-02-15 10:01:57 -0300

    pathogen configuration with plugins already defined

--- vim/pathogen-update.sh
+++ vim/pathogen-update.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
-
+#!/bin/bash
 cd ~/.vim/bundle
-for i in `ls`; do
-  cd "$i"
-  echo "Package $i -> "
-  git pull
-  cd ..
+(cat << EOF
+  https://github.com/tpope/vim-fugitive
+  https://github.com/tpope/vim-pathogen
+  https://github.com/tpope/vim-cucumber
+  https://github.com/junegunn/vim-easy-align
+  https://github.com/airblade/vim-gitgutter
+  https://github.com/ycm-core/YouCompleteMe
+  https://github.com/vim-airline/vim-airline
+  
+EOF
+) | \
+while read rep ;do
+    dir=${rep##*/}
+    echo "* $dir - $rep"
+    if [[ -d $dir ]] ;then
+        git -C $dir pull
+    else
+        git clone $rep
+    fi
 done
+
