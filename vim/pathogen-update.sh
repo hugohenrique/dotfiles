ed607b344c9b0f5c39f53f1c8f509af72b0ede3c
Hugo Henrique <contato@hugohenrique.com.br>
2021-08-04 14:12:49 -0300

    ...

--- vim/pathogen-update.sh
+++ vim/pathogen-update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 cd ~/.vim/bundle
 (cat << EOF
+  https://github.com/ctrlpvim/ctrlp.vim
   https://github.com/tpope/vim-fugitive
   https://github.com/tpope/vim-pathogen
   https://github.com/tpope/vim-cucumber
   https://github.com/junegunn/vim-easy-align
   https://github.com/airblade/vim-gitgutter
-  https://github.com/ycm-core/YouCompleteMe
   https://github.com/vim-airline/vim-airline
+  https://github.com/vim-airline/vim-airline-themes
+  https://github.com/preservim/nerdtree.git
+  https://github.com/Xuyuanp/nerdtree-git-plugin
 EOF
 ) | \
 while read rep ;do
