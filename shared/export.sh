685dcbdb9deca8ffcd36eac03138d6853e016914
Hugo Henrique <contato@hugohenrique.com.br>
2022-06-27 17:58:48 -0300

    ...

--- shared/export.sh
+++ shared/export.sh
@@ -3,20 +3,6 @@
 # ------------------------------------------------------------------------------
 GIT_COMPLETION_FILE=/usr/share/git-core/contrib/completion/git-prompt.sh
 
-# ------------------------------------------------------------------------------
-# Setup environment variables
-# ------------------------------------------------------------------------------
-export PATH=~/.npm-packages/bin:$PATH
-if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-  export PATH=~/.npm-packages/bin:~/.pyenv/bin:~/.local/bin:~/.config/composer/vendor/bin:$PATH
-  export JAVA_HOME=/etc/alternatives/java_sdk
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  export PATH=~/.composer/vendor/bin:$PATH
-  if [ -f /usr/libexec/java_home ]; then
-    export JAVA_HOME=$(/usr/libexec/java_home)
-  fi
-fi
-
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export EDITOR=vim
@@ -25,11 +11,6 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 export LS_COLORS="exfxcxdxcxegedabagacad"
 export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.yarn/bin:/usr/local/opt/gpg-agent/bin:$PATH"
 
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=~/.composer/vendor/bin:$PATH
 export LDFLAGS="-L/opt/homebrew/opt/mysql-client/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/mysql-client/include"
